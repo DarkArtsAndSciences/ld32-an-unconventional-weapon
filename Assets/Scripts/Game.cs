af6c6182e84cda016840e1889e77dd81f2049f4a
Dark Arts and Sciences <darkartsandsciences@gmail.com>
2015-04-20 13:16:02 -0700

    Removed unused scripts. Added game manager with NPC spawning.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public BoxCollider spawnZone;
+    public GameObject[] spawnTypes;
+    public int population = 3;
+
+	void Start () {
+        if (spawnZone == null) {
+            Debug.LogError("no spawn zone assigned");
+            return;
+        }
+        if (spawnTypes.Length == 0) {
+            Debug.LogError("no spawn types assigned");
+            return;
+        }
+
+        for (int i=0; i<population; i++)
+        {
+            GameObject prefab = spawnTypes[Random.Range(0,spawnTypes.Length)];
+            Vector3 position = new Vector3(
+                Random.Range(spawnZone.bounds.min.x, spawnZone.bounds.max.x), 
+                0, 
+                Random.Range(spawnZone.bounds.min.z, spawnZone.bounds.max.z));
+            Quaternion rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            Instantiate(prefab, position, rotation);
+        }
+	}
+	
+	void Update () {
+	
+	}
+}
